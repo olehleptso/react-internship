73e302bd135349c7d8671c28fe8dcc3766b4d158
Oleh <oleh.leptso@gmail.com>
2022-07-23 15:41:19 +0300

    header-info feature/api requests mock

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,19 +1,31 @@
 import './Header.css';
-import logo from '../../images/triangle.png'
+import logo from '../../images/triangle.png';
+import React, {useEffect, useState} from 'react';
+
+
 
 function Header () {
+    const [menuItems, setItems] = useState([]);
+
+    useEffect(()=>{
+        async function getHeaderItems() {
+            const response = await fetch('http://localhost:4000/header-menu');
+            const res = await response.json();
+            setItems(res);
+        }
+        getHeaderItems();
+    }, []);
+
     return (
             <div className="header">
                 <img src={logo} alt="Logo"/>
                 <div className="header-menu">
                     <ul className="header-nav">
-                        <li><a href="#">home</a></li>
-                        <li><a href="#">about</a></li>
-                        <li><a href="#">work</a></li>
-                        <li><a href="#">process</a></li>
-                        <li><a href="#">services</a></li>
-                        <li><a href="#">testimonials</a></li>
-                        <li><a href="#">contact</a></li>
+                        {menuItems.map((data) => (
+                            <li key={data.id}>
+                                <a>{data.name}</a>
+                            </li>
+                        ))}
                     </ul>
                 </div>
             </div>
