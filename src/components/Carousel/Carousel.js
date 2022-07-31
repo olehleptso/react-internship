e736f01cccd2263c55503f6b4212bf626560a40a
Oleh <oleh.leptso@gmail.com>
2022-07-31 14:48:38 +0300

    carousel sliding logic

--- src/components/Carousel/Carousel.js
+++ src/components/Carousel/Carousel.js
@@ -1,18 +1,39 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import CarouselItem from '../CarouselItem/CarouselItem';
 import './Carousel.css';
 
 function Carousel () {
+
+    let data = ["1","2","3"];
     const [currentSlide, setCurrentSlide] = useState(0);
 
+    const carouselScroll = () => {
+        if (currentSlide === data.length-1) {
+            return setCurrentSlide(0)
+        }
+        return setCurrentSlide(currentSlide + 1)
+    }
 
+    useEffect (() =>{
+        const interval = setInterval(()=> {carouselScroll()}, 2000)
+        return () => clearInterval(interval)
+    })
 
     return (
         <div className='carousel'>
             <div 
                 className='carousel-inner'
+                
             >
-                <CarouselItem/>
+                {data.map((item,index) => {
+                    return <div 
+                                className='carousel-item-block' key={index}
+                                style={{ transform: `translate(-${currentSlide * 100}%)`}}
+                            >
+                                <CarouselItem />
+                            </div>
+                })
+                }
             </div>
             
         </div>
--- src/components/CarouselItem/CarouselItem.js
+++ src/components/CarouselItem/CarouselItem.js
@@ -4,34 +4,34 @@ import Text from '../Text/Text';
 
 function CarouselItem () {
     return (
-        <div className='carousel-item'>
-            <div className='item-info-container'>
-                <div className='item-info'>
-                    <Text styles='title' text='Light Breakfast'/>
-                    <Text styles='gray' text='Litht his cant a creeping. Be, bring blessed night. Replenish blessed
-                    creature good. Saw earth every creepeth lights day, divided abundantly from. In. Said given lights
-                    Sixth the male. Upon their multiply. Kind best. Him tree Upon'/>
-                    <Text styles='gray' text='Cattle. Dominion day herb shed creeping divide darkness. Which. Subdue had'/>
+            <div className='carousel-item'>
+                <div className='item-info-container'>
+                    <div className='item-info'>
+                        <Text styles='title' text='Light Breakfast'/>
+                        <Text styles='gray' text='Litht his cant a creeping. Be, bring blessed night. Replenish blessed
+                        creature good. Saw earth every creepeth lights day, divided abundantly from. In. Said given lights
+                        Sixth the male. Upon their multiply. Kind best. Him tree Upon'/>
+                        <Text styles='gray' text='Cattle. Dominion day herb shed creeping divide darkness. Which. Subdue had'/>
+                    </div>
+                    <div className='item-info'>
+                        <div className='item-fields'>
+                            <Text styles='bold' text='Client:'/>
+                            <Text styles='gray' text='Emma Moris'/>
+                        </div>
+                        <div className='item-fields'>
+                            <Text styles='bold' text='Date:'/>
+                            <Text styles='gray' text='25.06.2017'/>
+                        </div>
+                        <div className='item-fields'>
+                            <Text styles='bold' text='Share:'/>
+                            <Text styles='gray' text='Facebook, Twitter, Pinterest'/>
+                        </div>
+                    </div>
                 </div>
-                <div className='item-info'>
-                    <div className='item-fields'>
-                        <Text styles='bold' text='Client:'/>
-                        <Text styles='gray' text='Emma Moris'/>
-                    </div>
-                    <div className='item-fields'>
-                        <Text styles='bold' text='Date:'/>
-                        <Text styles='gray' text='25.06.2017'/>
-                    </div>
-                    <div className='item-fields'>
-                        <Text styles='bold' text='Share:'/>
-                        <Text styles='gray' text='Facebook, Twitter, Pinterest'/>
-                    </div>
+                <div className='item-image'>
+                    <img src={breakfast}></img>
                 </div>
             </div>
-            <div className='item-image'>
-                <img src={breakfast}></img>
-            </div>
-        </div>
     )
 }
 
